013266211c519aa739b15aada97f4cefa808d8b2
RealBadAngel <maciej.kasatkin@o2.pl>
2014-05-21 12:16:35 +0200

    Update

--- item_entity.lua
+++ item_entity.lua
@@ -1,21 +1,31 @@
+-- If item_entity_ttl is not set, enity will have default life time 
+-- Setting it to -1 disables the feature
+
+local time_to_live = tonumber(minetest.setting_get("item_entity_ttl"))
+
+if not time_to_live then
+	time_to_live = 5
+end
+
+
 minetest.register_entity(":__builtin:item", {
 	initial_properties = {
 		hp_max = 1,
 		physical = true,
 		collide_with_objects = false,
-		collisionbox = {-0.17,-0.17,-0.17, 0.17,0.17,0.17},
+		collisionbox = {-0.24,-0.24,-0.24, 0.24,0.24,0.24},
 		visual = "sprite",
-		visual_size = {x=0.5, y=0.5},
+		visual_size = {x=0.3, y=0.3},
 		textures = {""},
 		spritediv = {x=1, y=1},
 		initial_sprite_basepos = {x=0, y=0},
 		is_visible = false,
-		play_flash = false,
 	},
-	
+
 	itemstring = '',
 	physical_state = true,
-
+	age = 0,
+	
 	set_item = function(self, itemstring)
 		self.itemstring = itemstring
 		local stack = ItemStack(itemstring)
@@ -25,7 +35,8 @@ minetest.register_entity(":__builtin:item", {
 			count = max_count
 			self.itemstring = stack:get_name().." "..max_count
 		end
-		local a = 0.15 + 0.15*(count/max_count)
+		local s = 0.15 + 0.15*(count/max_count)
+		local c = 0.8 * s
 		local itemtable = stack:to_table()
 		local itemname = nil
 		if itemtable then
@@ -41,18 +52,19 @@ minetest.register_entity(":__builtin:item", {
 			is_visible = true,
 			visual = "wielditem",
 			textures = {itemname},
-			visual_size = {x=a, y=a},
-			collisionbox = {-0.8*a,-0.8*a,-0.8*a, 0.8*a,0.8*a,0.8*a},
+			visual_size = {x=s, y=s},
+			collisionbox = {-c,-c,-c, c,c,c},
 			automatic_rotate = math.pi * 0.2,
 		}
 		self.object:set_properties(prop)
+		self.age = minetest.get_gametime()
 	end,
 
 	get_staticdata = function(self)
-		--return self.itemstring
 		return minetest.serialize({
 			itemstring = self.itemstring,
 			always_collect = self.always_collect,
+			age = self.age
 		})
 	end,
 
@@ -62,6 +74,11 @@ minetest.register_entity(":__builtin:item", {
 			if data and type(data) == "table" then
 				self.itemstring = data.itemstring
 				self.always_collect = data.always_collect
+				if data.age then 
+					self.age = data.age
+				else
+					self.age = minetest.get_gametime()
+				end
 			end
 		else
 			self.itemstring = staticdata
@@ -73,6 +90,11 @@ minetest.register_entity(":__builtin:item", {
 	end,
 
 	on_step = function(self, dtime)
+		if time_to_live > 0 and minetest.get_gametime() - self.age > time_to_live then
+			self.itemstring = ''
+			self.object:remove()
+			return
+		end
 		local p = self.object:getpos()
 		p.y = p.y - 0.3
 		local nn = minetest.get_node(p).name
@@ -81,7 +103,7 @@ minetest.register_entity(":__builtin:item", {
 		if not minetest.registered_nodes[nn] or minetest.registered_nodes[nn].walkable and v.y == 0 then
 			if self.physical_state then
 				local own_stack = ItemStack(self.object:get_luaentity().itemstring)
-				for _,object in ipairs(minetest.env:get_objects_inside_radius(p, 1)) do
+				for _,object in ipairs(minetest.get_objects_inside_radius(p, 1)) do
 					local obj = object:get_luaentity()
 					if obj and obj.name == "__builtin:item" and obj.physical_state == false then
 						local stack = ItemStack(obj.itemstring)
@@ -98,51 +120,49 @@ minetest.register_entity(":__builtin:item", {
 							local pos=object:getpos() 
 							pos.y = pos.y + (count - stack:get_count())/max_count * 0.15
 							object:moveto(pos, false)
-							--pos.y = pos.y + (count - stack:get_count())/max_count * 0.16
-							--self.object:moveto(pos, false)
-							local size
+							local s, c
 							local max_count = stack:get_stack_max()
-								if not overflow then
-									obj.itemstring = stack:get_name().." "..count
-									local size = 0.15 + 0.15*(count/max_count)
-									object:set_properties({
-										visual_size = {x=size, y=size},
-										collisionbox = {-0.8*size,-0.8*size,-0.8*size, 0.8*size,0.8*size,0.8*size}
-									})
-									self.object:remove()
-									return
-								else
-									size = 0.3
-									object:set_properties({
-										visual_size = {x=size, y=size},
-										collisionbox = {-0.8*size,-0.8*size,-0.8*size, 0.8*size,0.8*size,0.8*size}
-									})
-									obj.itemstring = stack:get_name().." "..max_count
-									size = 0.15 + 0.15*(count/max_count)
-									self.object:set_properties({
-										visual_size = {x=size, y=size},
-										collisionbox = {-0.8*size,-0.8*size,-0.8*size, 0.8*size,0.8*size,0.8*size}
-									})
-									self.itemstring = stack:get_name().." "..count
-								end
+							local name = stack:get_name()
+							if not overflow then
+								obj.itemstring = name.." "..count
+								s = 0.15 + 0.15*(count/max_count)
+								c = 0.8 * s
+								object:set_properties({
+									visual_size = {x=s, y=s},
+									collisionbox = {-c,-c,-c, c,c,c}
+								})
+								self.object:remove()
+								return
+							else
+								s = 0.3
+								c = 0.24
+								object:set_properties({
+									visual_size = {x=s, y=s},
+									collisionbox = {-c,-c,-c, c,c,c}
+								})
+								obj.itemstring = name.." "..max_count
+								s = 0.15 + 0.15*(count/max_count)
+								c = 0.8 * s
+								self.object:set_properties({
+									visual_size = {x=s, y=s},
+									collisionbox = {-c,-c,-c, c,c,c}
+								})
+								self.itemstring = name.." "..count
+							end
 						end
 					end
 				end
 				self.object:setvelocity({x=0,y=0,z=0})
 				self.object:setacceleration({x=0, y=0, z=0})
 				self.physical_state = false
-				self.object:set_properties({
-					physical = false
-				})
+				self.object:set_properties({physical = false})
 			end
 		else
 			if not self.physical_state then
 				self.object:setvelocity({x=0,y=0,z=0})
 				self.object:setacceleration({x=0, y=-10, z=0})
 				self.physical_state = true
-				self.object:set_properties({
-					physical = true
-				})
+				self.object:set_properties({physical = true})
 			end
 		end
 	end,
@@ -159,4 +179,5 @@ minetest.register_entity(":__builtin:item", {
 		self.object:remove()
 	end,
 })
+
 
